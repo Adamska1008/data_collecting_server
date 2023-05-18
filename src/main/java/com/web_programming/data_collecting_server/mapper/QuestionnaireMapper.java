937938ba228a26f97fc5184ecdbfc43c0bb2ab2c
adam <2639980868@qq.com>
2023-05-18 23:18:54 +0800

    feat:添加了对于mqtt中Pulish和Connection报文的处理

--- src/main/java/com/web_programming/data_collecting_server/mapper/QuestionnaireMapper.java
+++ src/main/java/com/web_programming/data_collecting_server/mapper/QuestionnaireMapper.java
@@ -2,8 +2,10 @@ package com.web_programming.data_collecting_server.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.web_programming.data_collecting_server.entity.Questionnaire;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Base64;
 
+@Mapper
 public interface QuestionnaireMapper extends BaseMapper<Questionnaire> {
 }
--- src/main/java/com/web_programming/data_collecting_server/mqtt/BootNettyMqttChannelInboundHandler.java
+++ src/main/java/com/web_programming/data_collecting_server/mqtt/BootNettyMqttChannelInboundHandler.java
@@ -1,22 +1,27 @@
 package com.web_programming.data_collecting_server.mqtt;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.web_programming.data_collecting_server.entity.SensorData;
+import com.web_programming.data_collecting_server.mapper.QuestionnaireMapper;
+import com.web_programming.data_collecting_server.mapper.SensorDataMapper;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.handler.codec.mqtt.MqttFixedHeader;
-import io.netty.handler.codec.mqtt.MqttMessage;
+import io.netty.handler.codec.mqtt.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.sql.Date;
 
 @Slf4j
 @Component
 public class BootNettyMqttChannelInboundHandler extends ChannelInboundHandlerAdapter {
+    @Autowired
+    SensorDataMapper sensorDataMapper;
     /*
-    从客户端接受新数据时调用这个方法
+    从客户端接受新数据时会调用这个方法
      */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception, IOException {
@@ -26,18 +31,57 @@ public class BootNettyMqttChannelInboundHandler extends ChannelInboundHandlerAda
             MqttFixedHeader header = message.fixedHeader();
             Channel channel = ctx.channel();
             switch (header.messageType()) {
-                case PUBLISH -> {
-                    readPublish(message);
-
-                }
+                case CONNECT -> con_ack(channel, message);
+                case PUBLISH -> publish_ack(channel, message);
             }
         }
     }
 
-    public void readPublish(MqttMessage message) {
-        JSONObject payload = (JSONObject) message.payload();
-        int SaO2 = payload.getIntValue("SaO2");
-        int PR = payload.getIntValue("PR");
+    public void con_ack(Channel channel, MqttMessage message) {
+        var connectMessage = (MqttConnectMessage) message;
+        var mqttFixedHeader = connectMessage.fixedHeader();
+        var mqttConnectVariableHeader = connectMessage.variableHeader();
+        var mqttConnAckVariableHeaderBack = new MqttConnAckVariableHeader(MqttConnectReturnCode.CONNECTION_ACCEPTED, mqttConnectVariableHeader.isCleanSession());
+        var mqttFixedHeaderBack = new MqttFixedHeader(MqttMessageType.CONNACK, mqttFixedHeader.isDup(), MqttQoS.AT_MOST_ONCE, mqttFixedHeader.isRetain(), 0x02);
+        var connAck = new MqttConnAckMessage(mqttFixedHeaderBack, mqttConnAckVariableHeaderBack);
+        log.debug("connection ack, back: " + connAck);
+        channel.writeAndFlush(connAck);
+    }
 
+    public void publish_ack(Channel channel, MqttMessage message) {
+        // 像数据库中存储数据
+        JSONObject payload = (JSONObject) message.payload();
+        log.debug("read publish message, payload: " + payload);
+        SensorData sensorData = new SensorData();
+        sensorData.setSensorId(payload.getIntValue("sensor_id"));
+        sensorData.setAcquisitionTime(payload.getSqlDate("acquisition_time"));
+        sensorData.setRealTimeData(payload.getString("read_time_data"));
+        sensorDataMapper.insert(sensorData);
+
+        var mqttPublishMessage = (MqttPublishMessage) message;
+        var mqttFixedHeader = mqttPublishMessage.fixedHeader();
+        var qos = (MqttQoS) mqttFixedHeader.qosLevel();
+        var headBytes = new byte[mqttPublishMessage.payload().readableBytes()];
+        mqttPublishMessage.payload().readBytes(headBytes);
+        var data = new String(headBytes);
+        switch (qos) {
+            case AT_LEAST_ONCE -> {
+                var mqttMessageIdVariableHeaderBack = MqttMessageIdVariableHeader.from(mqttPublishMessage.variableHeader().packetId());
+                //	构建返回报文， 固定报头
+                var mqttFixedHeaderBack = new MqttFixedHeader(MqttMessageType.PUBACK,mqttFixedHeader.isDup(), MqttQoS.AT_MOST_ONCE, mqttFixedHeader.isRetain(), 0x02);
+                //	构建PUBACK消息体
+                var pubAck = new MqttPubAckMessage(mqttFixedHeaderBack, mqttMessageIdVariableHeaderBack);
+                log.info("back--"+pubAck);
+                channel.writeAndFlush(pubAck);
+            }
+            case EXACTLY_ONCE -> {
+                //	构建返回报文， 固定报头
+                var mqttFixedHeaderBack = new MqttFixedHeader(MqttMessageType.PUBREC,false, MqttQoS.AT_LEAST_ONCE,false,0x02);
+                //	构建返回报文， 可变报头
+                var mqttMessageIdVariableHeaderBack = MqttMessageIdVariableHeader.from(mqttPublishMessage.variableHeader().packetId());
+                var mqttMessageBack = new MqttMessage(mqttFixedHeaderBack,mqttMessageIdVariableHeaderBack);
+                log.info("back--"+mqttMessageBack.toString());
+            }
+        }
     }
 }
